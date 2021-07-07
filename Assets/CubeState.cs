7c442adef92feabbe09083474c4628103a908f41
berryhermans <berryhermans@gmail.com>
2021-07-07 20:53:20 +0200

    face rotation

--- Assets/CubeState.cs
+++ Assets/CubeState.cs
@@ -27,4 +27,15 @@ public class CubeState : MonoBehaviour
         cubeSide[4].transform.parent.GetComponent<PivotRotation>().Rotate(cubeSide);
     }
 
+    public void PutDown(List<GameObject> littleCubes, Transform pivot)
+    {
+        foreach (GameObject littleCube in littleCubes)
+        {
+            if (littleCube != littleCubes[4])
+            {
+                littleCube.transform.parent.transform.parent = pivot;
+            }
+        }
+    }
+
 }
--- Assets/PivotRotation.cs
+++ Assets/PivotRotation.cs
@@ -10,19 +10,28 @@ public class PivotRotation : MonoBehaviour
     private List<GameObject> activeSide;
     private Vector3 localForward;
     private Vector3 mouseRef;
-    private bool dragging = false;
-    private float sensitivity = 0.4f;
+    private bool isDragging = false;
+    private bool isAutoRotating = false;
+    private float sensitivity = 0.1f;
+    private float speed = 300f;
     private Vector3 rotation;
+
+    private Quaternion targetQuaternion;
 
     void Update()
     {
-        if (dragging)
+        if (isDragging)
         {
             SpinSide(activeSide);
             if (Input.GetMouseButtonUp(0))
             {
-                dragging = false;
+                isDragging = false;
+                RotateToRightAngle();
             }
+        }
+        if (isAutoRotating)
+        {
+            AutoRotate();
         }
     }
 
@@ -30,10 +39,22 @@ public class PivotRotation : MonoBehaviour
     {
         activeSide = side;
         mouseRef = Input.mousePosition;
-        dragging = true;
+        isDragging = true;
 
         // create a vector to rotate around
         localForward = Vector3.zero - side[4].transform.parent.transform.localPosition;
+    }
+
+    public void RotateToRightAngle()
+    {
+        Vector3 vec = transform.localEulerAngles;
+        // round vec to nearest 90 degress
+        vec.x = Mathf.Round(vec.x / 90) * 90;
+        vec.y = Mathf.Round(vec.y / 90) * 90;
+        vec.z = Mathf.Round(vec.z / 90) * 90;
+
+        targetQuaternion.eulerAngles = vec;
+        isAutoRotating = true;
     }
 
     private void SpinSide(List<GameObject> side)
@@ -48,11 +69,49 @@ public class PivotRotation : MonoBehaviour
         {
             rotation.z = (mouseOffset.x + mouseOffset.y) * sensitivity * -1;
         }
+        if (side == cubeState.Back)
+        {
+            rotation.z = (mouseOffset.x + mouseOffset.y) * sensitivity * 1;
+        }
+        if (side == cubeState.Up)
+        {
+            rotation.y = (mouseOffset.x + mouseOffset.y) * sensitivity * 1;
+        }
+        if (side == cubeState.Down)
+        {
+            rotation.y = (mouseOffset.x + mouseOffset.y) * sensitivity * -1;
+        }
+        if (side == cubeState.Left)
+        {
+            rotation.x = (mouseOffset.x + mouseOffset.y) * sensitivity * -1;
+        }
+        if (side == cubeState.Right)
+        {
+            rotation.x = (mouseOffset.x + mouseOffset.y) * sensitivity * 1;
+        }
 
         // rotate
         transform.Rotate(rotation, Space.Self);
 
         // store mouse
         mouseRef = Input.mousePosition;
+    }
+
+    private void AutoRotate()
+    {
+        isDragging = false;
+        float step = speed * Time.deltaTime;
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetQuaternion, step);
+
+        // if within onde degree, set angle to target agnle and end the rotation
+        if (Quaternion.Angle(transform.localRotation, targetQuaternion) <= 1)
+        {
+            transform.localRotation = targetQuaternion;
+            cubeState.PutDown(activeSide, transform.parent);
+            cubeReader.ReadState();
+
+            isAutoRotating = false;
+            isDragging = false;
+        }
     }
 }
