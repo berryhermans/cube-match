17b2eeb2d4979e372a9b4c1dbc9ec779bbc6b064
berryhermans <berryhermans@gmail.com>
2021-07-11 17:01:43 +0200

    fix cubemap to work with changing colours

--- Assets/CubeMap.cs
+++ Assets/CubeMap.cs
@@ -29,30 +29,7 @@ public class CubeMap : MonoBehaviour
         int i = 0;
         foreach (Transform map in side)
         {
-            if (face[i].name[0] == 'F')
-            {
-                map.GetComponent<Image>().color = Color.blue;
-            }
-            if (face[i].name[0] == 'B')
-            {
-                map.GetComponent<Image>().color = Color.green;
-            }
-            if (face[i].name[0] == 'L')
-            {
-                map.GetComponent<Image>().color = new Color(1, 0.5f, 0, 1);
-            }
-            if (face[i].name[0] == 'R')
-            {
-                map.GetComponent<Image>().color = Color.red;
-            }
-            if (face[i].name[0] == 'U')
-            {
-                map.GetComponent<Image>().color = Color.yellow;
-            }
-            if (face[i].name[0] == 'D')
-            {
-                map.GetComponent<Image>().color = Color.white;
-            }
+            map.GetComponent<Image>().color = face[i].GetComponent<CubeFace>().MeshMaterial.color;
             i++;
         }
     }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public CubePainter cubePainter;
+    [SerializeField] private CubePainter cubePainter;
+    [SerializeField] private CubeReader cubeReader;
 
     private void Start()
     {
         cubePainter.Init();
         cubePainter.AddNextMaterial();
         cubePainter.PaintRandomFace();
+
+        cubeReader.ReadState();
     }
 }
